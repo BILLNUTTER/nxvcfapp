f3aca334a646f69dc376c70a919be555269245d2
BILLNUTTER <omayiocalvin59@gmail.com>
2026-02-10 08:58:55 +0300

    Change TARGET_COUNT from 250 to 150

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { WHATSAPP_CONTACTS } from './whatsappData';
 
 import './index.css';
 
-const TARGET_COUNT = 250;
+const TARGET_COUNT = 150;
 const API_URL = 'https://nuttervcf-ccc911dbe67f.herokuapp.com';
 
 function Main() {
